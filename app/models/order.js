318f718b57f821cafa3ab554a2801e435a5d7386
Brendan Winter <bwinter@waypointco.com>
2015-04-12 08:50:52 -0400

    returns obj

--- app/models/order.js
+++ app/models/order.js
@@ -6,16 +6,16 @@ var Schema       = mongoose.Schema;
 var OrderSchema   = new Schema({
     table_number: Number,
     timestamp: Date,
-    order: {
-		item_name: String,
-    	quantity: Number,
-    	comments: String,
-    	price: Number,
-    	timestamp: Date,
-    	status: [ { status: String,  timestamp: Date} ],
-    	updates: [ { change: String, timestamp: Date} ],
-    	type: [ { type: String } ] 
-	},
+    order: [{ 
+		// item_name: String,
+  //   	quantity: Number,
+  //   	comments: String,
+  //   	price: Number,
+  //   	timestamp: Date,
+  //   	status: [ { status: String,  timestamp: Date} ],
+  //   	updates: [ { change: String, timestamp: Date} ],
+  //   	type: [ { type: String } ] 
+	}],
     server: String,
     open: Boolean,
     paid: Boolean,
